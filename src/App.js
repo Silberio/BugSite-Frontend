488129e5a94ff72de4b9dbd5a5bf4405e929f2dd
Erinn Bastias <silberio50006@gmail.com>
2019-06-10 12:00:10 +0200

    frontend commit

--- src/App.js
+++ src/App.js
@@ -6,21 +6,74 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>BUG SITE</h4>
       </header>
+      <div className="App-content">
+        <h1>CONTENT</h1>
+        <MyComponent />
+      </div>
+      <div className="App-header">
+        <APIFetchTest />
+      </div>
     </div>
   );
+}
+
+
+
+class MyComponent extends React.Component {
+  handleClick(e) {
+    e.preventDefault()
+
+    fetch('http://localhost:8080/bugs/')
+      .then(function(response) {
+      return response.json();
+        })
+        .then(function(myJson) {
+    console.log(myJson);
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <p>MOTH</p>
+        <img src={ require("./media/mothsample.png")} />
+        <button onClick={this.handleClick}> Log </button>
+      </div>
+      );
+  }
+}
+
+class APIFetchTest extends React.Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bugs: ["moth","bug"]
+    };
+  }
+
+  componentDidMount() {
+        fetch('http://localhost:8080/bugs/').then(
+      function(response) {
+      return response.json();
+        }).then();
+  }
+
+  render() {
+    return(
+      <div>
+        <h3>API Fetch Test</h3>
+        <ul>
+          {
+            this.state.bugs.map((bug, i) => (<li>{bug}</li>))
+          }
+        </ul>
+      </div>)
+  }
+
 }
 
 export default App;
